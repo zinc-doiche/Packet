c56c0ad1ba4c344294a4132a7d02bc4324d367a7
lc0709 <3dollee@gmail.com>
2023-10-26 07:01:18 +0900

    success with paperweight

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.9.10"
+    id ("org.jetbrains.kotlin.jvm") version "1.9.10"
     id("io.papermc.paperweight.userdev") version "1.5.5"
     id ("xyz.jpenilla.run-paper") version "2.1.0" // Adds runServer task for testing
 }
@@ -9,14 +9,13 @@ version = "1.0"
 
 repositories {
     mavenCentral()
-    maven("https://repo.dmulloy2.net/repository/public/")
+    maven("https://repo.papermc.io/repository/maven-public/")
+    //maven("https://mvn.lumine.io/repository/maven-public/") //nms
 }
 
 dependencies {
     paperweight.paperDevBundle("1.20.1-R0.1-SNAPSHOT")
-    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
     implementation("io.github.monun:kommand-api:3.1.7")
-    implementation("io.github.monun:tap-api:4.9.8")
 }
 
 //의존성 탐색하도록 설정(duplicatesStrategy 설정시 필요)
@@ -30,7 +29,6 @@ tasks {
         from(configurations.implementation.get()
             .filter{
                 !it.name.contains("kommand")
-                        && !it.name.contains("tap")
                         && !it.name.contains("kotlin-")
             }
             .map { if (it.isDirectory) it else zipTree(it) })
